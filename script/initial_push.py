8b4b9159f2766297d09f5f5fe96450eb50cf0567
Xinye Li <lixinye@gmail.com>
2017-03-08 23:57:04 +0000

    Added .ini to gitignore, and updated initial_push.py with condition for processing valid listings

--- script/initial_push.py
+++ script/initial_push.py
@@ -54,65 +54,70 @@ for l in listings:
 	
 	count += 1
 	print('\nProcessing listing ' + str(count) + ' of ' + str(len(listings)) + ' ...')
-	left = l.find_element_by_css_selector('div > div.listing-results-left > div')
-	right = l.find_element_by_css_selector('div > div.listing-results-right.clearfix')
-	footer = l.find_element_by_css_selector('div > div.listing-results-footer.clearfix')
 	
-	title.append(right.find_element_by_css_selector('h2 > a').text)
-	address.append(right.find_element_by_css_selector('span > a').text)
-	
-	lat_long = right.find_elements_by_css_selector('div > meta')
-	lat.append(lat_long[0].get_attribute('content'))
-	long.append(lat_long[1].get_attribute('content'))
-	
-	price_raw = right.find_element_by_css_selector('div > a').text
-	price_appendix = right.find_element_by_css_selector('div > a > span').text
-	price.append((price_raw[:(len(price_raw) - len(price_appendix))]).strip())
-	
-	desc_br.append(right.find_element_by_css_selector('div > p').text)
-	contact_num.append(footer.find_element_by_css_selector('div.listing-results-right > p > span > a > span').text)
-	
-	try:
-		beds = right.find_element_by_css_selector('h3 > span.num-icon.num-beds')
-		num_beds.append(beds.text)
-	except:
-		num_beds.append('')
-	
-	try:
-		baths = right.find_element_by_css_selector('h3 > span.num-icon.num-baths')
-		num_baths.append(baths.text)
-	except:
-		num_baths.append('')
-	
-	try:
-		receps = right.find_element_by_css_selector('h3 > span.num-icon.num-reception')
-		num_receps.append(receps.text)
-	except:
-		num_receps.append('')
+	# check if the li element is an actual listing by checking the data-listing-id attribute
+	if l.get_attribute('data-listing-id') == None:
+		continue
+	else:
+		left = l.find_element_by_css_selector('div > div.listing-results-left > div')
+		right = l.find_element_by_css_selector('div > div.listing-results-right.clearfix')
+		footer = l.find_element_by_css_selector('div > div.listing-results-footer.clearfix')
 		
-	list_date_raw = footer.find_element_by_css_selector('div.listing-results-left > p > small').text
-	list_date.append(list_date_re.findall(list_date_raw)[0])
-	
-	try:
-		stations = right.find_elements_by_css_selector('div.nearby_stations_schools.clearfix > ul > li')
-		station1_raw = stations[0].text
-		station1_tup = station_re.findall(station1_raw)[0]
-		station1.append(station1_tup[0])
-		station1_dist.append(station1_tup[1])
-		station2_raw = stations[1].text
-		station2_tup = station_re.findall(station2_raw)[0]
-		station2.append(station2_tup[0])
-		station2_dist.append(station2_tup[1])
-	except:
-		station1.append('')
-		station1_dist.append('')
-		station2.append('')
-		station2_dist.append('')
+		title.append(right.find_element_by_css_selector('h2 > a').text)
+		address.append(right.find_element_by_css_selector('span > a').text)
+		
+		lat_long = right.find_elements_by_css_selector('div > meta')
+		lat.append(lat_long[0].get_attribute('content'))
+		long.append(lat_long[1].get_attribute('content'))
+		
+		price_raw = right.find_element_by_css_selector('div > a').text
+		price_appendix = right.find_element_by_css_selector('div > a > span').text
+		price.append((price_raw[:(len(price_raw) - len(price_appendix))]).strip())
+		
+		desc_br.append(right.find_element_by_css_selector('div > p').text)
+		contact_num.append(footer.find_element_by_css_selector('div.listing-results-right > p > span > a > span').text)
+		
+		try:
+			beds = right.find_element_by_css_selector('h3 > span.num-icon.num-beds')
+			num_beds.append(beds.text)
+		except:
+			num_beds.append('')
+		
+		try:
+			baths = right.find_element_by_css_selector('h3 > span.num-icon.num-baths')
+			num_baths.append(baths.text)
+		except:
+			num_baths.append('')
+		
+		try:
+			receps = right.find_element_by_css_selector('h3 > span.num-icon.num-reception')
+			num_receps.append(receps.text)
+		except:
+			num_receps.append('')
+			
+		list_date_raw = footer.find_element_by_css_selector('div.listing-results-left > p > small').text
+		list_date.append(list_date_re.findall(list_date_raw)[0])
+		
+		try:
+			stations = right.find_elements_by_css_selector('div.nearby_stations_schools.clearfix > ul > li')
+			station1_raw = stations[0].text
+			station1_tup = station_re.findall(station1_raw)[0]
+			station1.append(station1_tup[0])
+			station1_dist.append(station1_tup[1])
+			station2_raw = stations[1].text
+			station2_tup = station_re.findall(station2_raw)[0]
+			station2.append(station2_tup[0])
+			station2_dist.append(station2_tup[1])
+		except:
+			station1.append('')
+			station1_dist.append('')
+			station2.append('')
+			station2_dist.append('')
 
-	
-	id_raw = l.get_attribute('data-listing-id')
-	id.append(id_raw)
-	url.append(listing_url_prefix + '/' + id_raw)
+		
+		id_raw = l.get_attribute('data-listing-id')
+		id.append(id_raw)
+		url.append(listing_url_prefix + '/' + id_raw)
 	
 
 chrome.quit()
