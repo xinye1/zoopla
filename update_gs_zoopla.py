7ff6fd43b4c193dcf0525df56a37660d9c513974
Xinye Li <xinye.li4@wipro.com>
2017-03-10 01:00:50 +0000

    Added __name__ == "__main__" to the main program

--- update_gs_zoopla.py
+++ update_gs_zoopla.py
@@ -2,24 +2,29 @@
 
 # first of all, check if there's a connecton to Google API
 from df2gspread import gspread2df as g2d
-print('Testing Google API connection...')
-sheets_id = '1i7wVz-uP9kgLwNRWyRBvmQ9TTuEx9i0xi23GfSA7sSw'
-sheet_name = 'northwood'
-try:
-	gs = g2d.download(sheets_id, sheet_name, row_names = False, col_names = True)
-except TimeoutError:
-	print('No connection, programme stops.')
-	import sys
-	sys.exit()
 
-print('Connected, continue...')
+def main():
+	print('Testing Google API connection...')
+	sheets_id = '1i7wVz-uP9kgLwNRWyRBvmQ9TTuEx9i0xi23GfSA7sSw'
+	sheet_name = 'northwood'
+	try:
+		gs = g2d.download(sheets_id, sheet_name, row_names = False, col_names = True)
+	except TimeoutError:
+		print('No connection, programme stops.')
+		import sys
+		sys.exit()
 
-import script.zoopla as zpl
+	print('Connected, continue...')
 
-print('Getting the latest from Zoopla...')
-full_zoopla = zpl.query_zoopla()
+	import script.zoopla as zpl
 
-# print('Getting the current Google Sheets...')
-# gs = zpl.get_latest_gs()
+	print('Getting the latest from Zoopla...')
+	full_zoopla = zpl.query_zoopla()
 
-zpl.upload_new_rows(existing_gs = gs, new_zoopla = full_zoopla)
+	# print('Getting the current Google Sheets...')
+	# gs = zpl.get_latest_gs()
+
+	zpl.upload_new_rows(existing_gs = gs, new_zoopla = full_zoopla)
+	
+if __name__ == '__main__':
+	main()
